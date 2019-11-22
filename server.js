3cb5f70831212ed2b29cdd0c6643dfeb826ef1f1
NewBalanse <coocies12@gmail.com>
2019-11-22 18:42:33 +0200

    add setting

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.set(port);
+
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log('Server running');
+});
